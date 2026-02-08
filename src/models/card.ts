77ec8f3dc6f8ac691c6de6ec8dfe113035eadf6d
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-02-08 09:48:22 -0500

    Add files via upload

---
+++ src/models/card.ts
@@ -0,0 +1,8 @@
+export type suit = 'hearts' | 'clubs' | 'diamonds' | 'spades';
+export type rank = 'A' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
+
+export interface card {
+    suit: suit;
+    rank: rank;
+    value: number;
+}
---
+++ src/models/deck.ts
@@ -0,0 +1,18 @@
+import { card, suit, rank } from "./card";
+
+const suits: suit [] = ['hearts', 'diamonds', 'clubs', 'spades'];
+const ranks: rank [] =  ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+export function createDeck(): card[] {
+    const deck: card[] = [];
+    for(const suit of suits){
+        for(const rank of ranks){
+            let value = 0;
+            if(rank === 'A') value = 11;
+            else if(rank === 'J' || rank === 'Q'|| rank === 'K') value = 10;
+            else value =  parseInt(rank);
+            deck.push({ suit, rank, value });
+        }
+    }
+    return deck;
+}
