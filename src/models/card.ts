83d7e965dc179f1e98a1a25d6055e57a2aaab5e9
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-02-13 11:48:17 -0500

    Delete src directory

--- src/models/card.ts
+++
@@ -1,8 +0,0 @@
-export type suit = 'hearts' | 'clubs' | 'diamonds' | 'spades';
-export type rank = 'A' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
-
-export interface card {
-    suit: suit;
-    rank: rank;
-    value: number;
-}
